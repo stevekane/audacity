2f6dd2754ef116b7fbb04ce5427e8b2b1be0e759
Steve Kane <kanesteven@gmail.com>
2016-09-12 12:22:47 -0500

    rethought the content of src/AudioSystem a lot

--- src/AudioSystem.ts
+++ src/AudioSystem.ts
@@ -1,90 +1,25 @@
-import { IGraph, EdgePairGraph } from './Graph'
+export function gainWith (ac: AudioContext, opts: { volume?: number }): GainNode {
+  const g = ac.createGain()
 
-export class AudioSystem extends EdgePairGraph<GraphNode> {
-  edges: Map<GraphNode, Set<GraphNode>>
-  vertices: GraphNode[]
-  context: AudioContext
+  if ( opts.volume ) g.gain.value = opts.volume
+  return g
+}
 
-  constructor(context: AudioContext, pairs: Iterable<{ src: GraphNode, dest: GraphNode }>) {
-    super(pairs)
-    for ( const e of this.edges.entries() ) {
-      e[1].forEach(dest => e[0].node.connect(dest.node))  
-    }
-    this.context = context
+export function play (channel: AudioNode, playing: AudioBufferSourceNode[], buffers: AudioBuffer[]): AudioBufferSourceNode[] {
+  const sources = [] as AudioBufferSourceNode[]
+
+  for ( const buffer of buffers ) {
+    const source = channel.context.createBufferSource() 
+
+    source.buffer = buffer
+    source.connect(channel)
+    source.start(0)
+    sources.push(source)
   }
-}
-
-export interface IVec2<T> { x: T, y: T }
-
-export interface INode {
-  position: IVec2<number>
-  id: number
-}
-export interface IGain extends INode { 
-  kind: 'Gain', 
-  node: GainNode 
-}
-export interface IAnalyser extends INode { 
-  kind: 'Analyser', 
-  node: AnalyserNode, 
-  frequencyData: Uint8Array 
-}
-export interface IBufferSource extends INode { 
-  kind: 'BufferSource', 
-  node: AudioBufferSourceNode 
-}
-export interface IDestination extends INode { 
-  kind: 'Destination', 
-  node: AudioDestinationNode
-}
-
-export type GraphNode = IGain | IAnalyser | IBufferSource | IDestination
-
-export class Gain implements IGain {
-  id: number
-  kind: 'Gain' = 'Gain'
-  node: GainNode
-  position: IVec2<number>
-  constructor(ac: AudioContext, position: IVec2<number>, opts: { volume?: number }) {
-    this.id = Math.floor(Math.random() * 10000)
-    this.position = position
-    this.node = ac.createGain()
-    if ( opts.volume ) this.node.gain.value = opts.volume
+  for ( const sound of playing ) {
+    console.log(sound)
+    sound.stop(0) 
+    sound.disconnect(channel)
   }
-}
-
-export class Analyser implements IAnalyser { 
-  id: number
-  kind: 'Analyser' = 'Analyser'
-  node: AnalyserNode 
-  position: IVec2<number>
-  frequencyData: Uint8Array
-  constructor(ac: AudioContext, position: IVec2<number>, opts: { fftSize?: number }) {
-    this.id = Math.floor(Math.random() * 10000)
-    this.position = position
-    this.node = ac.createAnalyser()
-    if ( opts.fftSize ) this.node.fftSize = opts.fftSize
-    this.frequencyData = new Uint8Array(this.node.frequencyBinCount)
-  }
-}
-
-export class Destination implements IDestination {
-  id: number
-  kind: 'Destination' = 'Destination'
-  node: AudioDestinationNode
-  position: IVec2<number>
-  constructor(ac: AudioContext, position: IVec2<number>) {
-    this.id = Math.floor(Math.random() * 10000)
-    this.position = position
-    this.node = ac.destination 
-  }
-}
-
-export function play (channel: AudioNode, buffer: AudioBuffer): AudioBufferSourceNode {
-  const source = channel.context.createBufferSource()
-
-  source.buffer = buffer
-  source.connect(channel)
-  source.start(0)
-  return source
+  return sources
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,37 @@
 /// <reference path="../typings/index.d.ts" />
+
 interface DebugWindow extends Window {
-  audioSystem?: AudioSystem
+  ac?: AudioContext
 }
 declare var window: DebugWindow
 
 import * as React from 'react'
 import * as DOM from 'react-dom'
 import { loadAudioFile } from './loading'
-import { AudioSystemGraph } from './render'
-import { AudioSystem, Gain, Analyser, Destination, play } from './AudioSystem'
+import { play, gainWith } from './AudioSystem'
 import { log } from './utils'
 
 const ROOT_EL = document.createElement('div')
 const ac = new AudioContext
-const g = new Gain(ac, { x: 0, y: 200 }, { volume: 0.1 })
-const a = new Analyser(ac, { x: 0, y: 100 }, { fftSize: 256 })
-const d = new Destination(ac, { x: 0, y: 0 })
-const audioSystem = new AudioSystem(ac, [
-  { src: g, dest: a }, 
-  { src: a, dest: d }
-])
+const g = gainWith(ac, { volume: 0.1 })
+const playBuffer = []
 
-loadAudioFile(ac, 'bg-music1.mp3')
-.then(bgMusic => {
-  window.audioSystem = audioSystem
-  play(g.node, bgMusic.buffer)
+g.connect(ac.destination)
+window.ac = ac
+Promise.all([
+  loadAudioFile(ac, 'bg-music1.mp3'),
+  loadAudioFile(ac, 'sound1.mp3')
+])
+.then(sounds => {
+  const playingBuffers = play(g, playBuffer, [ sounds[0].buffer ])
+
+  setTimeout(play, 1000, g, playingBuffers, [ sounds[1].buffer ])
   requestAnimationFrame(render)
 })
 ROOT_EL.style.height = '100%'
 document.body.appendChild(ROOT_EL)
 
 function render () {
-  a.node.getByteFrequencyData(a.frequencyData)
-  DOM.render(<AudioSystemGraph { ...audioSystem } />, ROOT_EL)
+  DOM.render(<div>Hello</div>, ROOT_EL)
   requestAnimationFrame(render)
 }
