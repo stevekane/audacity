83541566a98c5583ec403d5d945dd99d87354454
Steve Kane <kanesteven@gmail.com>
2016-09-12 22:33:32 -0500

    changed core to concurrency to not block main with file loads

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import * as DOM from 'react-dom'
 import { loadAudioFile } from './loading'
 import { FileHandle } from './File'
 import { ISound, play, gainWith } from './AudioSystem'
-import { log } from './utils'
+import { log, push } from './utils'
 
 interface IClock {
   start: number
@@ -16,6 +16,7 @@ interface IClock {
   previous: number
   dT: number
   elapsed: number
+  update(now: number): void
 }
 
 interface IState {
@@ -32,9 +33,17 @@ class Clock implements IClock {
   previous: number = Date.now()
   dT: number = 0
   elapsed: number = 0
+
+  update(now: number): void {
+    this.previous = this.current
+    this.current = now
+    this.dT = this.current - this.previous
+    this.elapsed += this.dT
+  }
 }
 
 const rootEl = document.createElement('div')
+const networkInbox = [] as FileHandle<AudioBuffer>[]
 const initialState: IState = {
   clock: new Clock,
   rootEl: rootEl,
@@ -51,28 +60,28 @@ async function forever<T> (f: (t: T) => Promise<T>, t: T): Promise<T> {
   return await f(t).then(t1 => forever(f, t1))
 }
 
-async function update (state: IState): Promise<IState> {
-  state.clock.previous = state.clock.current
-  state.clock.current = Date.now()
-  state.clock.dT = state.clock.current - state.clock.previous
-  state.clock.elapsed += state.clock.dT
+async function update (networkInbox: FileHandle<AudioBuffer>[], state: IState): Promise<IState> {
+  const audioFiles = networkInbox.splice(0)
+
+  if ( audioFiles.length ) push(state.playing, audioFiles.map(af => play(state.ac.destination, af.buffer)))
+
+  state.clock.update(Date.now())
   state.playing = state.playing.filter(s => s.playing)
+  state.audioFiles.concat(audioFiles)
   DOM.render(<div>{ `Sounds Playing: ${ state.playing.length }` } </div>, state.rootEl)
   return Promise.resolve(state)
 }
 
-async function main (state: IState): Promise<IState> {
+async function main (networkInbox: FileHandle<AudioBuffer>[], state: IState): Promise<IState> {
   const out = state.ac.destination
-  const rafUpdate = raf.bind(null, update)
-  const audioFiles = await Promise.all([ 
-    loadAudioFile(state.ac, 'bg-music1.mp3'), 
-    loadAudioFile(state.ac, 'sound1.mp3')
-  ])
-  
-  state.playing = audioFiles.map(a => play(out, a.buffer))
-  state.audioFiles = audioFiles
-  return forever(rafUpdate, state)
+  const partialUpdate = (s: IState) => update(networkInbox, s)
+  const partialRaf = (s: IState) => raf(partialUpdate, s)
+  const fileLoads = [ loadAudioFile(state.ac, 'bg-music1.mp3'), loadAudioFile(state.ac, 'sound1.mp3') ]
+  const addFile = (files: FileHandle<AudioBuffer>[]) => networkInbox.push(...files)
+
+  Promise.all(fileLoads).then(addFile)
+  return forever(partialRaf, state)
 }
 
 document.body.appendChild(rootEl)
-main(initialState)
+main(networkInbox, initialState)
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,6 @@
 export const log = console.log.bind(console)
 export const last = <A>(ar: A[]): A => ar[ar.length - 1]
+export const push = <A>(a: A[], b: A[]): A[] => {
+  a.push(...b)
+  return a
+}
